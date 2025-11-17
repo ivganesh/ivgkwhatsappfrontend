6be0060ae1b8dfa216c05abf43a2b957e44bb074
ivganesh <ivganeshface@gmail.com>
2025-11-17 23:11:12 +0530

    url change

--- lib/api/client.ts
+++ lib/api/client.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosInstance, InternalAxiosRequestConfig } from 'axios';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000/api';
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://whatsappcloud.ivgk.in/api';
 
 class ApiClient {
   private client: AxiosInstance;
